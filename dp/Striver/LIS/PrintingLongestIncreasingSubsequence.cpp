99e8ae2e4cc91238234302eb74c74f110b3b3644
Shobhit150 <vshobhit150@gmail.com>
2026-01-22 02:18:35 +0530

    print lis

--- dp/Striver/LIS/PrintingLongestIncreasingSubsequence.cpp
+++ dp/Striver/LIS/PrintingLongestIncreasingSubsequence.cpp
@@ -21,20 +21,26 @@ public:
     
     int tab(vector<int> &nums) {
         int n = nums.size();
-        vector<int> dp(n+1, 0);
+        vector<int> dp(n+1, 1);
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<i;j++) {
-                
+                if(nums[j] < nums[i]) {
+                    dp[i] = max(dp[i] , dp[j]+1);
+                }
             }
         }
-
+        int maxi = 1;
+        for(int i=0;i<n;i++) {
+            maxi = max(maxi, dp[i]);
+        }
+        return maxi;
     }
     int lengthOfLIS(vector<int>& nums) {
         vector<int> temp;
         int n = nums.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
-        return memo(nums, 0, -1, dp);
+        return tab(nums);
     }
 };
 
