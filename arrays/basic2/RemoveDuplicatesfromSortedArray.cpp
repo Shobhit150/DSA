dfb11f2d0392a2cd94d2bd58d4ef22a86cb32692
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-04-27 23:55:20 +0530

    ques: duplicate

--- arrays/basic2/RemoveDuplicatesfromSortedArray.cpp
+++ arrays/basic2/RemoveDuplicatesfromSortedArray.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int j = 0;
+        int n = nums.size();
+        for(int i=1;i<n;i++) {
+            if(nums[i] != nums[i-1]) {
+                // cout << i << " " << nums[i] << " " << nums[i-1] << " " << nums[j] << "\n";
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
     }
 };
