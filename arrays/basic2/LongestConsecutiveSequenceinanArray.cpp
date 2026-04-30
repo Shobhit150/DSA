7e353f87073b98e74fcd12b4e45ed593e4e70de0
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-04-30 22:19:04 +0530

    ques: spiral

--- arrays/basic2/LongestConsecutiveSequenceinanArray.cpp
+++ arrays/basic2/LongestConsecutiveSequenceinanArray.cpp
@@ -9,14 +9,17 @@ public:
             st.insert(nums[i]);
         }
 
-        for(int i=0;i<n;i++) {
-            int num = nums[i];
-            int count = 1;
-            while(st.find(num-1) != st.end()) {
-                count++;
-                num--;
+        for(auto num: st) {
+            if(st.find(num-1) == st.end()) {
+                int count = 1;
+                int curr = num;
+                while(st.find(curr+1) != st.end()) {
+                    count++;
+                    curr++;
+                }
+                maxi = max(maxi, count);
             }
-            maxi = max(maxi, count);
+            
         }
         return maxi;
     }
---
+++ arrays/basic2/SpiralMatrix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int cou = 0;
+        vector<int> res;
+        int i = 0;
+        int j = 0;
+        while(m >= 1 && n >= 1) {
+            if(cou%4 == 0) {
+                for(int i=1;i<n;i++) 
+            }
+        }
+    }
+};
