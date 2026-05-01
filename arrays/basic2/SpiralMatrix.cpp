489f0bedf81aea361b7a7814148f7b2d9fc921b0
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-01 07:36:38 +0530

    ques: spiral

--- arrays/basic2/SpiralMatrix.cpp
+++ arrays/basic2/SpiralMatrix.cpp
@@ -3,14 +3,36 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int cou = 0;
         vector<int> res;
-        int i = 0;
-        int j = 0;
-        while(m >= 1 && n >= 1) {
-            if(cou%4 == 0) {
-                for(int i=1;i<n;i++) 
+        int t = 0;
+        int b = m - 1;
+        int l = 0;
+        int r = n - 1;
+        while(b >= t && r >= l) {
+            for(int i=l;i<=r;i++) {
+                res.push_back(matrix[t][i]);
+            }
+            t++;
+
+            for(int i=t;i<=b;i++) {
+                res.push_back(matrix[i][r]);
+            }
+            r--;
+
+            if(t<=b) {
+                for(int i=r;i>=l;i--) {
+                    res.push_back(matrix[b][i]);
+                }
+                b--;
+            }
+
+            if(l<=r) {
+                for(int i=b;i>=t;i--) {
+                    res.push_back(matrix[i][l]);
+                }
+                l++;
             }
         }
+        return res;
     }
 };
