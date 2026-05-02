0cac5196485b4ed5535a0b998dc3184d48164f22
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-02 07:24:46 +0530

    majority 2

---
+++ arrays/basic2/MajorityElementII.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0;
+        int ele1 = INT_MIN;
+        int cnt2 = 0;
+        int ele2 = INT_MIN;
+
+        int n = nums.size();
+        for(int i=0;i<n;i++) {
+            if(ele1 == nums[i]) cnt1++;
+            else if(ele2 == nums[i]) cnt2++;
+            else if(cnt1 == 0) {
+                cnt1 = 1;
+                ele1 = nums[i];
+            } else if(cnt2 == 0) {
+                cnt2 = 1;
+                ele2 = nums[i];
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0;
+        cnt2 = 0;
+
+        for(int i=0;i<n;i++) {
+            if(nums[i] == ele1) cnt1++;
+            if(nums[i] == ele2) cnt2++;
+        }
+
+        vector<int> ans;
+        if(cnt1 > n/3) {
+            ans.push_back(ele1);
+        }
+        if(cnt2 > n/3) {
+            ans.push_back(ele2);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+};
