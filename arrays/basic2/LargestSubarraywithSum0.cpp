d6e15d151b6ef9cf5f11902d5afd104606d2331f
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-02 10:39:07 +0530

    ques: largest sumarray with 0

--- arrays/basic2/LargestSubarraywithSum0.cpp
+++ arrays/basic2/LargestSubarraywithSum0.cpp
@@ -1,6 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
 class Solution {
   public:
     int maxLen(vector<int>& arr) {
-        // Your code goes here
+        vector<int> pre;
+        int preSum = 0;
+        int n = arr.size();
+        unordered_map<int,int> mp;
+        mp[0] = -1;
+        int maxi = -1;
+
+        for(int i=0;i<n;i++) {
+            preSum += arr[i];
+            pre.push_back(preSum);
+            if(mp.find(preSum) != mp.end()) {
+                maxi = max(maxi, i - mp[preSum]);
+            } else {
+                mp[preSum] = i;
+            }
+        }
+
+        
+        return maxi;
     }
 };
+
+int main() {
+    Solution s;
+    vector<int> arr = { 15, -2, 2, -8, 1, 7, 10, 23 };
+    cout << s.maxLen(arr) << endl; 
+}
