03e6a3d9600bd0c8fae87d56df985ff6cd576b7b
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-05 01:28:40 +0530

    concept: insert in lower_bound

--- binarySearch/1D/Searchinsertposition.cpp
+++ binarySearch/1D/Searchinsertposition.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
+    int lower_bound2(vector<int> &nums, int x) {
+        int n = nums.size();
+        int l = 0;
+        int r = n;
+        while(l>r) {
+            int mid = (r+l)/2;
+            if(nums[mid] >= x) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
     int searchInsert(vector<int> &nums, int target)  {
-       
+       int idx = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+       return idx;
     }
 };
