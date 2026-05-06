2c2482f78f0adcf00bbf0ee6e6b8644bb5ffcc4a
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-05-07 00:18:57 +0530

    ques: search in rotated sorted array

--- binarySearch/1D/SearchinRotatedSortedArray.cpp
+++ binarySearch/1D/SearchinRotatedSortedArray.cpp
@@ -11,16 +11,16 @@ public:
             if(nums[mid] == target) return mid;
 
             if(nums[l] <= nums[mid]) {
-                if(target > nums[mid]) {
-                    l = mid + 1;
-                } else {
+                if(target < nums[mid] && target >= nums[l]) {
                     r = mid-1;
+                } else {
+                    l = mid+1;
                 }
             } else {
-                if(target < nums[mid]) {
-                    r = mid - 1;
-                } else {
+                if(target > nums[mid] && target <= nums[r]) {
                     l = mid + 1;
+                } else {
+                    r = mid - 1;
                 }
             }
         }
