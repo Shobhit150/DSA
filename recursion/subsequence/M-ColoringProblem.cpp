0f621f317de5a547879d3eca9dde00832432564d
Shobhit <104744652+Shobhit150@users.noreply.github.com>
2026-08-04 01:18:46 +0530

    ques: word break complete

---
+++ recursion/subsequence/M-ColoringProblem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    bool recur(int v, vector<vector<int>> &edges, int i, int m, vector<int> &color) {
+        if(i==v) return true;
+
+        
+
+    }
+    bool graphColoring(int v, vector<vector<int>> &edges, int m) {
+        vector<int> color(v,-1);
+
+    }
+};
+
+void solve() {
+    
+}
+
+int main() {
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
--- recursion/subsequence/WordBreak.cpp
+++ recursion/subsequence/WordBreak.cpp
@@ -4,8 +4,27 @@ using namespace std;
 
 class Solution {
 public:
+    bool recur(string &s, vector<string>& wordDict, int i, vector<int> &mem) {
+        if(i > s.size()) return false;
+        if(i==s.size()) return true;
+        if(mem[i] != -1) {
+            if(mem[i] == 0) return false;
+            return true;
+        }
+        for(int k=0;k<wordDict.size();k++) {
+            if(s.substr(i, wordDict[k].size()) == wordDict[k]) {
+                if(recur(s, wordDict, i+wordDict[k].size(),mem)) {
+                    mem[i] = 1;
+                    return true;
+                };
+            }
+        }
+        mem[i] = 0;
+        return false;
+    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        
+        vector<int> mem(s.size()+1,-1);
+        return recur(s,wordDict,0, mem);
     }
 };
 
